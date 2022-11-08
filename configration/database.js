5f0015712ca7342e25f1ba90b5e960db753436b2
TahaShek <sohailtaha128@gmail.com>
2022-11-08 16:31:20 +0500

    comit

--- configration/database.js
+++ configration/database.js
@@ -5,6 +5,8 @@ mongoose.connect(`${process.env.MONGO_URI}`,{ useNewUrlParser:true, useUnifiedTo
     if (!error) {
         console.log('MDataBase');
         // console.log("Your App Has the Following Dependicies\n");
+        for( dep in Package.dependencies)
+        console.log(dep)
         // for (dependencies in Package.dependencies) {
             // console.log(dependencies);
         // }

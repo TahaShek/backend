07a72ee52c42fcd08d39b396b0e213a0bb4cfa8c
TahaShek <sohailtaha128@gmail.com>
2022-11-12 16:59:47 +0500

    comit

--- middlewares/media-middleware.js
+++ middlewares/media-middleware.js
@@ -3,7 +3,7 @@ const multor=require('multer')
 const crypto=require('crypto')
 const fs =require('fs')
  
-// fucnction for the refrence name 
+// fucnction for the refrence name ....basically this function converts the filename into hex form 
 const hashfunc=(filename)=>{
 const hash =crypto.createHash('md5')
 hash.update(filename)

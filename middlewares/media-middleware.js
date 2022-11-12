82b815132c45db98046ac0d5cae0b7655a91a4a4
TahaShek <sohailtaha128@gmail.com>
2022-11-12 17:13:46 +0500

    comit

--- middlewares/media-middleware.js
+++ middlewares/media-middleware.js
@@ -10,3 +10,41 @@ hash.update(filename)
 const md5Summary=hash.digest('hex')
 return md5Summary
 }
+
+
+// store image path funciton ....handle its extension etc 
+let UploadImage =multor({
+    storage:multor.diskStorage({
+        destination:(req,next,cb)=>{
+            let path=`./assests/Product/${req.body.productName}`;
+            if(!fs.existsSync(path)){
+                fs.mkdirSync(path,function(err,res){
+             if(err){
+                res.json(err)
+             }
+             else{
+                res.json('Saved Succefully');
+
+             }
+                })
+            }
+            cb(null,path);
+        },
+        filename:(req,file,cb)=>{
+            const md5Summary=hashfunc(file.originalname)
+            //originalname is the uploaded file's name with date iso String
+          let ext=file.mimetype.split('/')[1]
+           // Fix svg+xml bug
+           if (ext.includes('svg')) {
+            ext = 'svg';
+        }
+        
+        cb(null,`${Date.now()}_${md5Summary}.${ext}`)
+
+        }
+    })
+})
+
+
+
+module.exports={UploadImage}
